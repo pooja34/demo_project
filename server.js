8b806113decf948247f875ad8cf19f9021dd96d4
pooja <pooja.bhandare@sunbeaminfo.com>
2020-02-01 12:20:33 +0530

    add POST /route

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ app.get('/', (request, response) => {
     response.send('welcome to my backend')
 })
 
+app.post('/', (request, response) => {
+    response.send('welcome to post')
+})
 app.listen(9898, '0.0.0.0', () => {
     console.log('server started on port 9898')
 })
